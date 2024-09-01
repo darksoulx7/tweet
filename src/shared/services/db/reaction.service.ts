16552f43a6c06084dbe4862cbcffd0b2b2afa92d
darksoulx7 <devrepo89@gmail.com>
2024-09-01 17:51:46 +0530

    feat: implemented post reaction feature

--- src/shared/services/db/reaction.service.ts
+++ src/shared/services/db/reaction.service.ts
@@ -66,35 +66,23 @@ class ReactionService {
     ]);
   }
 
-  public async getPostReactions(
-    query: IQueryReaction,
-    sort: Record<string, 1 | -1>,
-  ): Promise<[IReactionDocument[], number]> {
+  public async getPostReactions(query: IQueryReaction, sort: Record<string, 1 | -1>): Promise<[IReactionDocument[], number]> {
     const reactions: IReactionDocument[] = await ReactionModel.aggregate([
-      { $match: query },
-      { $sort: sort },
+      { $match: query }, { $sort: sort },
     ]);
     return [reactions, reactions.length];
   }
 
-  public async getSinglePostReactionByUsername(
-    postId: string,
-    username: string,
-  ): Promise<[IReactionDocument, number] | []> {
-    const reactions: IReactionDocument[] = await ReactionModel.aggregate([
-      {
-        $match: {
-          postId: new mongoose.Types.ObjectId(postId),
-          username: Helpers.firstLetterUppercase(username),
-        },
-      },
-    ]);
+  public async getSinglePostReactionByUsername(postId: string, username: string): Promise<[IReactionDocument, number] | []> {
+    const reactions: IReactionDocument[] = await ReactionModel.aggregate([{
+      $match: {
+        postId: new mongoose.Types.ObjectId(postId),
+        username: Helpers.firstLetterUppercase(username),
+      }}]);
     return reactions.length ? [reactions[0], 1] : [];
   }
 
-  public async getReactionsByUsername(
-    username: string,
-  ): Promise<IReactionDocument[]> {
+  public async getReactionsByUsername(username: string): Promise<IReactionDocument[]> {
     const reactions: IReactionDocument[] = await ReactionModel.aggregate([
       { $match: { username: Helpers.firstLetterUppercase(username) } },
     ]);
