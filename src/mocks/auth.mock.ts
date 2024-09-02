2396eb50ca6a47557e919f9f42b8517eac267fa3
darksoulx7 <devrepo89@gmail.com>
2024-09-02 23:41:35 +0530

    test commit

--- src/mocks/auth.mock.ts
+++ src/mocks/auth.mock.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { Response } from 'express';
 import { AuthPayload, IAuthDocument } from '@auth/interfaces/auth.interface';
 
